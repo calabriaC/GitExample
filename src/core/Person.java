e77c74ac7b163f2df84723549f64fac905121edd
Carlos Miguel Calabria Patiño <calabriac@uninorte.edu.co>
2025-03-17 15:31:08 -0500

    Feat: Added Person class

---
+++ src/core/Person.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author calabriac
+ */
+public class Person {
+    private long ID;
+    private String name;
+    private int age;
+
+    public Person(long ID, String name, int age) {
+        this.ID = ID;
+        this.name = name;
+        this.age = age;
+    }
+    
+    
+    
+}
